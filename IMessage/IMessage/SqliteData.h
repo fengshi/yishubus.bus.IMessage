ec77d56b936c8a2604db9156702c26651d6c2a5d
shifeng <shifeng007@gmail.com>
2013-06-04 17:58:57 +0800

    no message

---
+++ IMessage/IMessage/SqliteData.h
@@ -0,0 +1,13 @@
+//
+//  SqliteData.h
+//  IMessage
+//
+//  Created by shi feng on 13-6-4.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SqliteData : NSObject
+
+@end
---
+++ IMessage/IMessage/SqliteData.m
@@ -0,0 +1,13 @@
+//
+//  SqliteData.m
+//  IMessage
+//
+//  Created by shi feng on 13-6-4.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import "SqliteData.h"
+
+@implementation SqliteData
+
+@end
