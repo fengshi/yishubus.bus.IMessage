7971834ec60a47637118df577ef09922a6b367e7
shifeng <shifeng007@gmail.com>
2013-06-08 12:15:11 +0800

    no message

---
+++ IMessage/IMessage/TeacherDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TeacherDetailViewController.h
+//  IMessage
+//
+//  Created by shi feng on 13-6-8.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TeacherDetailViewController : UIViewController
+
+@end
---
+++ IMessage/IMessage/TeacherDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  TeacherDetailViewController.m
+//  IMessage
+//
+//  Created by shi feng on 13-6-8.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import "TeacherDetailViewController.h"
+
+@interface TeacherDetailViewController ()
+
+@end
+
+@implementation TeacherDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
