2ba6abff7c2f722383c679c54be16b91c9eb5b7b
shifeng <shifeng007@gmail.com>
2013-06-17 17:11:13 +0800

    no message

---
+++ IMessage/IMessage/ShowMessageCell.h
@@ -0,0 +1,13 @@
+//
+//  ShowMessageCell.h
+//  IMessage
+//
+//  Created by shi feng on 13-6-17.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowMessageCell : UITableViewCell
+
+@end
---
+++ IMessage/IMessage/ShowMessageCell.m
@@ -0,0 +1,29 @@
+//
+//  ShowMessageCell.m
+//  IMessage
+//
+//  Created by shi feng on 13-6-17.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import "ShowMessageCell.h"
+
+@implementation ShowMessageCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
