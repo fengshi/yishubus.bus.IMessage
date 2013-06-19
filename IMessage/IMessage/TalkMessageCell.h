36366f2e29608c4546aa855a07227e131f1a889c
shifeng <shifeng007@gmail.com>
2013-06-19 18:17:08 +0800

    no message

---
+++ IMessage/IMessage/TalkMessageCell.h
@@ -0,0 +1,13 @@
+//
+//  TalkMessageCell.h
+//  IMessage
+//
+//  Created by shi feng on 13-6-19.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TalkMessageCell : UITableViewCell
+
+@end
---
+++ IMessage/IMessage/TalkMessageCell.m
@@ -0,0 +1,29 @@
+//
+//  TalkMessageCell.m
+//  IMessage
+//
+//  Created by shi feng on 13-6-19.
+//  Copyright (c) 2013年 yishubus. All rights reserved.
+//
+
+#import "TalkMessageCell.h"
+
+@implementation TalkMessageCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
